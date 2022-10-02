130b892236a3084d51a9a43e30618de919fa9396
Sandeep Kumar <sandeep335577@gmail.com>
2022-10-02 18:27:46 +0530

    Add Utilities Class

---
+++ BharatPHP/Utilities.php
@@ -0,0 +1,28 @@
+<?php
+
+class Utilities {
+
+    public static function encrypt($string, $key = 5) {
+        $result = '';
+        for ($i = 0, $k = strlen($string); $i < $k; $i++) {
+            $char = substr($string, $i, 1);
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) + ord($keychar));
+            $result .= $char;
+        }
+        return base64_encode($result);
+    }
+
+    public static function decrypt($string, $key = 5) {
+        $result = '';
+        $string = base64_decode($string);
+        for ($i = 0, $k = strlen($string); $i < $k; $i++) {
+            $char = substr($string, $i, 1);
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) - ord($keychar));
+            $result .= $char;
+        }
+        return $result;
+    }
+
+}
